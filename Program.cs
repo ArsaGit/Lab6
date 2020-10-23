e4a730de61b5a111bc4dc745b6902f88f12d2bb9
Artyom <artyom.arsalanov@gmail.com>
2020-10-23 23:00:40 +0800

    v2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Lab6
 {
@@ -6,59 +8,78 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            int[] a1 = new int[] { 1, 2 ,3,4,5};
-            int[] a2 = new int[] { 1, 2 };
-            int[] a3 = new int[] { 1, 3 };
+            int[] a = new int[] { 1, 2, 3, 4, 5 };
+            int[] a6 = new int[] { 1, 2, 3, 4, 5 };
+            int[] a1 = new int[] { 2, 3 };
+            int[] a2 = new int[] { 1, 2, 4 };
+            int[] a3 = new int[] { 2, 6 };
+
+            int[,] z = { { 1, 2, 3 }, { 3, 4, 5 }, { 7, 8, 9 }};
+            int[,] z1 = { { 3, 4, 5 }, { 7, 8, 9 }};
+            int[,] z5 = { { 1, 2, 3 }, { 3, 4, 5 }};
+            int[,] z2 = { { 3, 4, 5 } };
+            int[] z3 = { 1, 2, 3 };
+            int[,] z4 = { { 3, 4, 5 }, { 6,7, 8} };
 
 
-            int[][] aa1 = new int[3][];
-            aa1[0] = new int[] { 1, 2, 3 };
-            aa1[1] = new int[] { 3, 4, 5 };
-            aa1[2] = new int[] { 7, 8, 9 };
+            ShowResult(IsPartOf(a, a1));
+            ShowResult(IsPartOf(a, a2));
+            ShowResult(IsPartOf(a, a3));
+            ShowResult(IsPartOf(z, z1));
+            ShowResult(IsPartOf(z, z2));
+            ShowResult(IsPartOf(z, z3));
+            ShowResult(IsPartOf(z, z4));
 
-            int[][] aa2 = new int[2][];
-            aa2[0] = new int[] { 3, 4, 5 };
-            aa2[1] = new int[] { 7, 8, 9 };
-
-            ShowResult(IsPartOf(aa1, aa2));
-
-            
 
         }
+        
         static void ShowResult(bool res)
         {
             if (res) Console.WriteLine("true");
             else Console.WriteLine("false");
         }
-        static bool IsPartOf(int[] arrBig,int[] arrSmall)
+        static bool IsPartOf(int[] arrBig, int[] arrSmall)
         {
             int count;
-            for(int i=0;i<arrBig.Length-arrSmall.Length+1;i++)
+            for (int i = 0; i < arrBig.Length - arrSmall.Length + 1; i++)
             {
                 count = 0;
-                for(int j=0;j<arrSmall.Length;j++)
-                    if (arrBig[i+j] == arrSmall[j]) count++;
+                for (int j = 0; j < arrSmall.Length; j++)
+                    if (arrBig[i + j] == arrSmall[j]) count++;
                 if (count == arrSmall.Length) return true;
             }
             return false;
         }
-
-        static bool IsPartOf(int[][] arrBig, int[] arrSmall)
+        static int[] GetArray1D(int[,] array2D, int index = 0)
         {
-            for (int i = 0; i < arrBig.GetUpperBound(0)+1; i++)
-                if (IsPartOf(arrBig[i], arrSmall)) return true;
+            int size = array2D.GetLength(1);
+            var array1D = new int[size];
+            for (int i = 0; i < size; i++)
+                array1D[i] = array2D[index, i];
+            return array1D;
+        }
+        static bool IsPartOf(int[,] arrBig, int[] arrSmall)
+        {
+            for (int i = 0; i < arrBig.GetLength(0); i++)
+            {
+                int[] array1 = GetArray1D(arrBig, i);
+                if (IsPartOf(array1, arrSmall)) return true;
+            }
             return false;
         }
-
-        static bool IsPartOf(int[][] arrBig, int[][] arrSmall)
+        static bool IsPartOf(int[,] arrBig, int[,] arrSmall)
         {
             int count;
-            for (int i = 0; i < arrBig.GetUpperBound(0) - arrSmall.GetUpperBound(0)+1; i++)
+            for (int i = 0; i < arrBig.GetLength(0) - arrSmall.GetLength(0)+1; i++)
             {
                 count = 0;
-                for (int j = 0; j < arrSmall.GetUpperBound(0)+1; j++)
-                    if (IsPartOf(arrBig[i + j],arrSmall[j])) count++;
-                if (count == arrSmall.GetUpperBound(0)+1) return true;
+                for (int j = 0; j < arrSmall.GetLength(0); j++)
+                {
+                    int[] array1 = GetArray1D(arrBig, i + j);
+                    int[] array2 = GetArray1D(arrSmall, j);
+                    if (IsPartOf(array1, array2)) count++;
+                }
+                if (count == arrSmall.GetLength(0)) return true;
             }
             return false;
         }
